4096dccefe8c6895e77f895b2e8410465ff7b2ea
Patrick Lafrance <plafrance@crosemont.qc.ca>
2021-02-25 13:28:35 -0500

    ajout CI

---
+++ publish.el
@@ -0,0 +1,26 @@
+;; publish.el --- Publish org-mode project on Gitlab Pages
+;; Author: Rasmus
+
+;;; Commentary:
+;; This script will convert the org-mode files in this directory into
+;; html.
+
+(setq org-publish-project-alist
+      (list
+       (list "org"
+             :base-directory "org"
+             :base-extension 'any
+             :publishing-directory "public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+       (list "src"
+             :base-directory "src"
+             :base-extension 'any
+             :publishing-directory "public"
+             :publishing-function 'org-publish-attachment
+             :recursive t)
+	   ))
+
+(provide 'publish)
+;;; publish.el ends here
+
